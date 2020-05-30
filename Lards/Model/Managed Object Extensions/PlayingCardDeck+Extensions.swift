beb5bdbd3318b6890d74f961e3bde6053292d522
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-05-29 23:28:50 -0500

    Implement rest of CoreData objects

--- Lards/Model/Managed Object Extensions/PlayingCardDeck+Extensions.swift
+++ Lards/Model/Managed Object Extensions/PlayingCardDeck+Extensions.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+extension PlayingCardDeck {
+   public override func awakeFromInsert() {
+      Suit.allCases.forEach { suit in
+         Rank.allCases.forEach { rank in
+            let newCard = PlayingCard(context: self.managedObjectContext!)
+            newCard.rank = rank.rawValue
+            newCard.suit = suit.rawValue
+         }
+      }
+   }
+}
